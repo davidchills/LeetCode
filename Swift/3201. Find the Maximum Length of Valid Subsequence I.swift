1d01aa0156afa808e6e9881e597a2af2e911530e
Dave Hills <daveh@dailyhills.com>
2025-07-15 17:59:42 -0700

    2025-07-15

---
+++ Swift/3201. Find the Maximum Length of Valid Subsequence I.swift
@@ -0,0 +1,34 @@
+/*
+You are given an integer array nums.
+A subsequence sub of nums with length x is called valid if it satisfies:
+(sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] + sub[x - 1]) % 2.
+Return the length of the longest valid subsequence of nums.
+A subsequence is an array that can be derived from another array by deleting some or 
+    no elements without changing the order of the remaining elements.
+    
+3201. Find the Maximum Length of Valid Subsequence I    
+*/
+class Solution {
+    func maximumLength(_ nums: [Int]) -> Int {
+        var odd = 0
+        var even = 0
+        var oddEven = 0
+        var evenOdd = 0
+        for num in nums {
+            if num % 2 == 0 {
+                odd += 1
+                oddEven = evenOdd + 1
+            }
+            else {
+                even += 1
+                evenOdd = oddEven + 1
+            }
+        }
+        return max(odd, even, oddEven, evenOdd)
+    }
+}
+let solution = Solution()
+print(solution.maximumLength([1,2,3,4])) // Expect 4
+print(solution.maximumLength([1,2,1,1,2,1,2])) // Expect 6
+print(solution.maximumLength([1,3])) // Expect 2
+print(solution.maximumLength([1,8,4,2,4])) // Expect 4
